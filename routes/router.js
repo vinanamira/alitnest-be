e535a50ee60a98a0031f3ef98bb7c766af9d7e86
vinanamira <mzssdaru@gmail.com>
2025-03-28 02:46:42 +0700

    Reset daily goals

--- routes/router.js
+++ routes/router.js
@@ -21,7 +21,7 @@ router.put('/password/:id', userController.updatePassword);
 router.post('/nutriwise', upload.single('file'), nutriwise.analyzeFood);
 
 // ============== Daily Goals ===============
-// router.post('/food', dailyGoalsController.createDailyGoals);
+
 
 // ============== In App Survey ===============
 router.post('/survey', survey.createSurvey);
