1a2ddb87694728ac3109c484879dcabf78f99d7d
vinanamira <mzssdaru@gmail.com>
2025-03-30 18:35:53 +0700

    Mengedit port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
