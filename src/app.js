ed7cee407724a4eabaf2559756861ba9e8ea18f8
vinanamira <mzssdaru@gmail.com>
2025-03-30 20:58:57 +0700

    Connect ke database

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import 'dotenv/config'; 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
