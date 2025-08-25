04d1d672d2bcc505bd6bd035ec50ce97dba33ae5
vinanamira <mzssdaru@gmail.com>
2025-08-25 17:59:00 +0700

    Survey: Change variabel names

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -21,8 +21,8 @@ export const createSurvey = async (req, res) => {
 
 export const getSurveyById = async (req, res) => {
     try {
-        const { id } = req.params;
-        const survey = await Survey.findById(id);
+        const { userId } = req.params;
+        const survey = await Survey.findById(userId);
         if (!survey) {
             return res.status(404).json({
                 status: "FAILED",
